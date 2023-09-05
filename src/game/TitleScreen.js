61b683636ac36baab5682f901ba5fd2a56a160d7
Zachary Rankin <zach@rknmail.com>
2023-09-05 15:47:41 +0000

    Experiment with some sounds

--- src/game/TitleScreen.js
+++ src/game/TitleScreen.js
@@ -1,3 +1,5 @@
+import "../zzfx.js";
+
 let isInitialized = false;
 function TitleScreen({ startGame }) {
   if (!isInitialized) {
@@ -11,6 +13,8 @@ function TitleScreen({ startGame }) {
     `;
     const startBtn = document.querySelector("#start-button");
     startBtn.addEventListener("click", () => {
+      /* prettier-ignore */
+      zzfxP(...zzfxM(...[[[,0,400,,.27,,1]],[[[,-1,16,19,21,14,11,9,11,14,16,16,14,16,19,16,14,16,16,14,16,19,19,19,16,,14,,9,11,9,7,7,7,9,11,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,],[,1,,19,,16,16,,16,16,9,7,4,4,4,4,4,,,11,9,14,16,14,4,2,4,4,4,4,4,4,4,4,4,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,],[,-1,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,],[,1,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,]]],[0],55,{"title":"New Song","instruments":["Instrument 0"],"patterns":["Pattern 0"]}]));
       startGame();
     });
     isInitialized = true;
